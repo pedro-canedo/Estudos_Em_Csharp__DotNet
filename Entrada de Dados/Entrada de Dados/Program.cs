a862a5c5882fbb9c439a6a477ec2d3140cc7c2e2
Pedro Augusto Canedo Araujo Obalhe <82132100+pedro-canedo@users.noreply.github.com>
2021-10-01 08:48:05 -0300

    Compreendendo as entradas de dados em c# Aula02

---
+++ Entrada de Dados/Entrada de Dados/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Entrada_de_Dados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual seu nome ? ");
+            string nome = Console.ReadLine();
+            Console.Write("Qual a sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+            Console.Write("Digite seu Sexo(M)(F) Ou (N): ");
+            char sexo = char.Parse(Console.ReadLine());
+            Console.Write("Qual a sua altura: ");
+            double altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Console.WriteLine("Confirmando dados");
+            Console.WriteLine($"Seu nome é {nome}");
+            Console.WriteLine($"Sua idade é {idade}");
+            Console.WriteLine($"Sua altura é {altura}cm");
+            Console.WriteLine($"Seu sexo é {sexo}");
+
+
+
+        }
+    }
+}
