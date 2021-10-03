5929732f39180d4a9b712fbd9abe4c878ff55945
Pedro Augusto Canedo Araujo Obalhe <82132100+pedro-canedo@users.noreply.github.com>
2021-10-03 06:12:07 -0300

    Trabalhando com funções e debug

---
+++ Aula debug/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Aula_debug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            double resultado = Maior(n1, n2, n3);
+
+            Console.WriteLine($"O maior numéro digitado é:{resultado}");
+        }
+
+        static int Maior(int a, int b, int c)
+        {
+            int m;
+            if (a > b && a > c)
+            {
+                m = a;
+            } else if (b > c)
+            {
+                m = b;
+            } else
+            {
+                m = c;
+            }
+            return m;
+        }
+    }
+}
+
