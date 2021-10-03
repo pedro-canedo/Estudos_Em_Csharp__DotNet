63beba768f7891aa7670d837a8fb1d62d93cbe88
Pedro Augusto Canedo Araujo Obalhe <82132100+pedro-canedo@users.noreply.github.com>
2021-10-03 08:04:59 -0300

    Estrutura de repetição

---
+++ Estrutura repetitiva while/Estrutura repetitiva while/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+
+namespace Estrutura_repetitiva_while
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String continuar = null;           
+            Console.Write("Deseja iniciar o programa? 'S' para sim e 'N' para não: ");
+            continuar = Console.ReadLine().ToUpper();
+            while (continuar == "S")
+            {
+                Console.Write("Digite um número: ");
+                double x = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                double raiz = Math.Sqrt(x);
+                Console.WriteLine($"A raiz quadrada deste número é: {raiz.ToString("F2", CultureInfo.InvariantCulture)}");
+                Console.WriteLine("Continuar ?: 'S' para sim e 'N' para não");
+                continuar = Console.ReadLine().ToUpper();
+            }
+            Console.WriteLine("Programa encerrado");
+        }
+    }
+}
