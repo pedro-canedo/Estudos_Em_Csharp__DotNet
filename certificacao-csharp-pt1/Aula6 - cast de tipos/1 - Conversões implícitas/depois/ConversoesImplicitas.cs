c845d11b603f187e6a4c23c137789a35e7d12177
Pedro Augusto Canedo Araujo Obalhe <82132100+pedro-canedo@users.noreply.github.com>
2021-10-11 09:12:13 -0300

    Trilha para certificação C# programing Microsoft

---
+++ certificacao-csharp-pt1/Aula6 - cast de tipos/1 - Conversões implícitas/depois/ConversoesImplicitas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace certificacao_csharp_roteiro
+{
+    class ConversoesImplicitas : IAulaItem
+    {
+        public void Executar()
+        {
+            int inteiro = 2_123_456_789;
+            long inteiroLongo = (long)inteiro;
+            Console.WriteLine(inteiroLongo);
+
+            int inteiroLongo1 = (int)inteiroLongo;
+            inteiro = inteiroLongo1;
+        }
+    }
+}
+
